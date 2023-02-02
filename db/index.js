726fd32d1ce8c5f10bb3be05abcc87bfa29a9e65
Aman Arabi <amanarabi@gmail.com>
2023-02-02 11:03:45 +0300

    updated

--- db/index.js
+++ db/index.js
@@ -34,28 +34,54 @@ let Answers = mongoose.model('answer', answerSchema);
 let Photos = mongoose.model('photo', photoSchema);
 
 let getQuestions = (prod_id)=>{
-  var clientObj = {}
+  //intitiate object to send back to client
+  var clientObj = {product_id: prod_id, results: []}
+  var thisObj;
+  //get questions from db
    var ques =  Questions.aggregate(
     [ { $match : { product_id : Number(prod_id) } }
      ])
 
 Promise.all([ques]).then((questions) => {
   var answerPromises = []
-  console.log("qqqq", questions, "qqqq")
+  //for each question get answers
    questions[0].forEach((quest) => {
-    //update clientObj with ques info
+    //transform reported to true and false
+    var report = false;
+    if(quest.reported === 1){
+      report = true;
+    }
+    //build client obj with question info
+    thisObj = {
+      question_id: quest.id,
+      question_body: quest.body,
+      question_date: quest.date_written, //transform date
+      asker_name: quest.asker_name,
+      question_helpfulness: quest.helpful,
+      reported: report, //
+      answers: {}
+    }
+    //answers from db
     var ans = Answers.aggregate(
       [{$match : {question_id: quest.id}}
     ])
     answerPromises.push(ans)
-   })
+  })
    Promise.all(answerPromises).then((answers) => {
-    console.log("wwww", answers, "wwww")
     var answerPhotosPromises = []
+    //for each answer get photos
     answers.forEach((answ) => {
       answ.forEach((answer)=>{
-        console.log("yyyy", answer, "yyyy")
-        ////update clientObj with answers info
+        //build client obj with answer info
+        thisObj.answers[answer.id.toString()] = {
+          id: answer.id,
+          body: answer.body,
+          date: answer.date, //trnasform date
+          answerer_name: answer.answerer_name,
+          helpfulness: answer.helpful,
+          photos: []
+        }
+        //get photos from db
         var photo = Photos.aggregate(
           [{$match : {answer_id: answer.id}}
         ])
@@ -64,15 +90,27 @@ Promise.all([ques]).then((questions) => {
       })
     })
     Promise.all(answerPhotosPromises).then((photos) => {
-     console.log("OOO", photos, "OOO")
-     ////update clientObj with answersphotos info
+      //for each photo get url
+     photos.forEach((phot) => {
+      if(phot !== []){
+      phot.forEach((photoObj) => {
+          thisObj.answers[photoObj.answer_id.toString()].photos.push(phot.url)
+      })
+    }
+     })
+     clientObj.results.push(thisObj)
+
+   })
+
+   .then(()=>{
+    console.log(clientObj, "clinettttttt")
+    return clientObj
 
    })
 
    })
-});
-
-console.log(clientObj, "clinettttttt")
+})
+console.log(clientObj, "ttttt")
 //return clientOBJ inside promise
 }
 
