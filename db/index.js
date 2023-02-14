cf84870426dfa012ffd48730969e545cb79c7e84
Aman Arabi <amanarabi@gmail.com>
2023-02-14 23:53:29 +0300

    get requests optimal

--- db/index.js
+++ db/index.js
@@ -54,18 +54,9 @@ let setQuestionSchema = new mongoose.Schema({
     question_body: String,
     question_date: { type: Date },
     asker_name: String,
-    question_helpfulness: String,
+    question_helpfulness: Number,
     reported: Boolean,
-    answers: {
-      String: {
-        id: Number,
-        body: String,
-        date: { type: Date } ,
-        answerer_name: String,
-        helpfulness: Number,
-        photos: [],
-      }
-    }
+    answers: {}
   }]
 })
 
@@ -78,71 +69,33 @@ let Photos = mongoose.model('photo', photoSchema);
 
 
 let getQuestions = async (prod_id) => {
-  //intitiate object to send back to client
-  var clientObj = { product_id: prod_id, results: [] }
-  //get questions from db
-  var questions = await Questions.aggregate(
+
+  var questions = await SetQuestions.aggregate(
     [
-      { $match: { product_id: Number(prod_id) } }
+      { $match: { product_id: prod_id.toString() } }
     ]
   )
+  return questions[0]
 
-  //for each question get answers
-  for (const question of questions) {
-
-    //transform reported to true and false
-    var report = false;
-    if (question.reported === 1) {
-      report = true;
-    }
-    //build client obj with question info
-    var curQuestion = {
-      question_id: question.id,
-      question_body: question.body,
-      question_date: new Date(question.date_written).toISOString(), //transform date
-      asker_name: question.asker_name,
-      question_helpfulness: question.helpful,
-      reported: report, //
-      answers: {}
-    }
-    //answers from db
-    var answers = await Answers.aggregate(
-      [{ $match: { question_id: question.id } }
-      ])
-
-    for (const answer of answers) {
-      //build client obj with answer info
-      curQuestion.answers[answer.id.toString()] = {
-        id: answer.id,
-        body: answer.body,
-        date: answer.date, //trnasform date
-        answerer_name: answer.answerer_name,
-        helpfulness: answer.helpful,
-        photos: []
-      }
-      //get photos from db
-      var photos = await Photos.aggregate(
-        [{ $match: { answer_id: answer.id } }
-        ])
-      //for each photo get url
-      if (photos !== []) {
-        for (const photoObj of photos) {
-
-          curQuestion.answers[photoObj.answer_id.toString()].photos.push(photoObj.url)
-        }
-      }
-    }
-    clientObj.results.push(curQuestion)
-  }
-  return clientObj;
 }
 
 let postQuestions  = async (data) => {
+  console.log(data, "innsss")
   let counting = await Questions.countDocuments({}, function( err, count){
     return count
   })
+ let questionInfo = {
+    question_id: counting +1,
+    question_body: data.body,
+    question_date: Math.floor(new Date().getTime() / 1000),
+    asker_name: data.name,
+    question_helpfulness: 0,
+    reported: false,
+  }
+  let updated = await SetQuestions.findOneAndUpdate({product_id: data.product_id.toString()}, {"$push": { "results": questionInfo } })
+  console.log(updated, "updatedddd")
   var obj = {
-    id: counting +1,
+    question_id: counting +1,
     product_id: data.product_id,
     body: data.body,
     date_written: Math.floor(new Date().getTime() / 1000),
@@ -161,41 +114,14 @@ let helpfulQuestion  = async (questionId) => {
 }
 
 let AnswersGet = async (questionId) => {
-  var clientObj = { question: questionId,
-                    page: 1,
-                    count: 100,
-                    results: [] }
-
-  var answers = await Answers.aggregate(
-    [{ $match: { question_id: Number(questionId) } }
+  var answers = await SetAnswers.aggregate(
+    [{ $match: { question: Number(questionId) } }
     ])
 
-  for (const answer of answers) {
-
-    var curAnswer = {
-      answer_id: answer.id,
-      body: answer.body,
-      date: new Date(answer.date_written).toISOString(),
-      answerer_name: answer.answerer_name,
-      helpfulness: answer.helpful,
-      photos: []
-    }
-    var photos = await Photos.aggregate(
-      [{ $match: { answer_id: answer.id } }
-      ])
-    //for each photo get url
-    if (photos !== []) {
-      for (const photoObj of photos) {
-        curAnswer.photos.push({id: photoObj.id, url: photoObj.url})
-      }
-    }
-
-    clientObj.results.push(curAnswer)
-  }
-
-return clientObj;
+  return answers[0];
 
 }
+
 let postAnswer = async (questionId, data) => {
   let counting = await Answers.countDocuments({}, function( err, count){
     return count
@@ -334,7 +260,6 @@ let setQuestions = async () => {
       }
     }
     clientObj.results.push(curQuestion)
-    console.log(clientObj, "kkk")
   }
   let created = await SetQuestions.create(clientObj)
   }
